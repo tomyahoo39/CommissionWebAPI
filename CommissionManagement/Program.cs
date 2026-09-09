851096708872f647e0f29abaf480d4e398873991
tomyahoo39 <tomyahoo39@gmail.com>
2026-09-09 14:18:56 +0800

    更改前端的Cors設定

--- CommissionManagement/Program.cs
+++ CommissionManagement/Program.cs
@@ -29,7 +29,14 @@ builder.Services.AddScoped<ICommissionTypeService, CommissionTypeService>();
 builder.Services.AddScoped<ICommissionPeriodService, CommissionPeriodService>();
 builder.Services.AddScoped<IImagesService, ImagesService>();
 builder.Services.AddScoped<IImageDatabaseService, ImageDatabaseService>();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:/5173").AllowAnyHeader().AllowAnyMethod();
+        });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,7 +48,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowFrontend");
 app.UseAuthorization();
 
 app.MapControllers();
