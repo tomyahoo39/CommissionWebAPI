f3e06fd6c41bef9c5bfe447eb21477815785d3d6
KULIMU <tomyahoo39@gmail.com>
2026-09-20 21:51:37 +0800

    把開發環境拔掉，讓部署伺服器可以測試Swagger

--- CommissionManagement/Program.cs
+++ CommissionManagement/Program.cs
@@ -131,12 +131,12 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+    
+app.MapOpenApi();  
+app.UseSwagger();  
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
