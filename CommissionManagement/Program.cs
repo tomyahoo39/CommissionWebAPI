438fb6d1877304cba58ff39e5920898cb4ee9e36
tomyahoo39 <tomyahoo39@gmail.com>
2026-09-22 15:04:02 +0800

    調整開發人員才看的到swagger 不外露出

--- CommissionManagement/Program.cs
+++ CommissionManagement/Program.cs
@@ -132,11 +132,12 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-    
-app.MapOpenApi();  
-app.UseSwagger();  
-app.UseSwaggerUI();
-
+if(app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();  
+    app.UseSwagger();  
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
